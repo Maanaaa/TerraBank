85ae04bbe004ca9e801eb987d3ad49fd841fa86a
Maanaaa <manya.th@icloud.com>
2023-07-13 10:28:07 +0200

    Adding class Class DatabaseManager added Database connection test added

--- src/main/java/fr/mana/terrabank/TerraBank.java
+++ src/main/java/fr/mana/terrabank/TerraBank.java
@@ -3,12 +3,13 @@ package fr.mana.terrabank;
 import fr.mana.terrabank.loading.*;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class TerraBank extends JavaPlugin {
+public final class Main extends JavaPlugin {
 
     @Override
     public void onEnable() {
         // Initialize plugin
         PluginInitializer pluginInitializer = new PluginInitializer(this);
+        pluginInitializer.register();
     }
 
     @Override
