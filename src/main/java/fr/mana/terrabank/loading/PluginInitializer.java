a8ce1c1b32feb1e611e0bdc89e8dcdae710eba70
Maanaaa <manya.th@icloud.com>
2023-07-13 10:38:55 +0200

    Class creation and Command creation Reload class /terrabank reload command created

--- src/main/java/fr/mana/terrabank/loading/PluginInitializer.java
+++ src/main/java/fr/mana/terrabank/loading/PluginInitializer.java
@@ -1,6 +1,7 @@
 package fr.mana.terrabank.loading;
 
 import fr.mana.terrabank.*;
+import fr.mana.terrabank.commands.*;
 
 public class PluginInitializer {
 
@@ -14,6 +15,7 @@ public class PluginInitializer {
         DatabaseManager databaseManager = new DatabaseManager(main);
         main.saveDefaultConfig();
         databaseManager.connect();
+        main.getCommand("terrabank").setExecutor(new Reload(main));
     }
 
 }
